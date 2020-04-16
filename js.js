26d66cd59e0d0f1a4e02e235f08732747dafab72
Ira <moby.dick.killer@gmail.com>
2020-04-16 19:48:50 +0300

    add drag

--- js.js
+++ js.js
@@ -1,18 +1,65 @@
-let puzzleParent=$("#puzzle");
-let puzzleItems = $(".puzzle-item");
+$(document).ready(function () {
 
-let filed=$("#filed");
-let filedItems=$(".filed-item");
+    let puzzleParent = $("#puzzle");
+    let puzzleItems = $(".puzzle-item");
 
-$(function () {
+    let field = $("#field");
+    let fieldItems = $(".field-item");
+
     puzzleItems.each(function (item) {
-        $(this).css('background', `url("img/british/britich${item+1}.jpg") no-repeat center / cover`);
+        $(this).css('background', `url("img/british/britich${item + 1}.jpg") no-repeat center / cover`);
     });
-});
 
-$(function () {
     while (puzzleItems.length) {
-        let randomImg=Math.floor(Math.random() * puzzleItems.length);
-        puzzleParent.append(puzzleItems.splice(randomImg, 1)[0]);
+        let randomIndexImg = Math.floor(Math.random() * puzzleItems.length);
+        let temp = puzzleItems.splice(randomIndexImg, 1)[0];
+        puzzleParent.append(temp);
     }
-});
+
+    // drag&drop
+
+    function handleDragStart(e) {
+        $(this).css('opacity', 0.5);
+    }
+
+    function handleDragEnd(e) {
+        $(this).css('opacity', 1);
+    }
+
+
+    puzzleItems = $(".puzzle-item");
+
+    puzzleItems.each(function () {
+        this.addEventListener('dragstart', handleDragStart, false);
+        this.addEventListener('dragend', handleDragEnd, false);
+    })
+
+    fieldItems.each(function () {
+        this.addEventListener('dragover', handleDragOver, false);
+        this.addEventListener('dragenter', handleDragEnter, false);
+        this.addEventListener('dragleave', handleDragLeave, false);
+    })
+
+    //Task 2
+
+    function handleDragOver(e) {
+        if (e.preventDefault) {
+            e.preventDefault(); // Necessary. Allows us to drop.
+        }
+
+        e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+        return false;
+    }
+
+    function handleDragEnter(e) {
+        $(e.target).addClass('over');
+    }
+
+
+    function handleDragLeave(e) {
+        $(e.target).removeClass('over');
+    }
+
+
+})
